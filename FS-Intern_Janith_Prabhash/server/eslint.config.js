5c1d1d960e4ae0d44feba8cf7f7d6db60c45f9a3
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-09-07 23:17:45 +0530

    Update eslint.config.js

--- FS-Intern_Janith_Prabhash/server/eslint.config.js
+++ FS-Intern_Janith_Prabhash/server/eslint.config.js
@@ -3,11 +3,11 @@ import js from '@eslint/js';
 export default [
   js.configs.recommended,
   {
-    ignores: ['dist/**', 'node_modules/**'],
+  ignores: ['dist/**', 'node_modules/**', 'prisma/**/*.js'],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
-      globals: { process: 'readonly' },
+      globals: { process: 'readonly', console: 'readonly' },
     },
     rules: {
       'no-console': 'off',
